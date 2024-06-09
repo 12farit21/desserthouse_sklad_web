fb62db1011c090ac5f76021644a2c1ceaf45e0a6
Nurzhan Zhumatayev <12farit21@gmail.com>
2024-06-10 02:05:18 +0500

    asdasd

--- frontend/my-frontend/src/config.js
+++ frontend/my-frontend/src/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const API_URL = "https://136.244.95.57:8000";
-=======
-export const API_URL = "http://136.244.95.57:5000/";
->>>>>>> 5136565fab6dbc53f7560e8f8067801ad6b35819
+export const API_URL = "https://136.244.95.57:5000";
